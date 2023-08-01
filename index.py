6b17f29b94700422d33ed50ee4313cb51ab434c0
Veregorn <rjbarco@gmail.com>
2023-08-01 17:35:30 +0200

    Now guessed letters are replaced in display var

--- index.py
+++ index.py
@@ -4,14 +4,19 @@ r = RandomWords()
 # Return a single random word
 randomWord = r.get_random_word()
 
-print(randomWord)
+# Create a list with "_" instead of letters. Length must be the same as random word length
+display = []
+for i in range(0,len(randomWord)):
+    display.append("_")
+
+print(f"Pssst, the solution is {randomWord}.")
 
 # Ask player for a letter
 guess = input("Guess a letter: ").lower()
 
 # Print right or wrong depending on letter coincidence inside selected random word
-for letter in randomWord:
-    if letter == guess:
-        print("Right")
-    else:
-        print("Wrong")
+for i in range(0,len(randomWord)):
+    if randomWord[i] == guess:
+        display[i] = guess
+
+print(display)
