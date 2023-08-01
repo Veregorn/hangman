73acdaa1693ad69366ba8dd1a2328ed347977283
Veregorn <rjbarco@gmail.com>
2023-08-01 17:10:50 +0200

    Compare player letter with every letter in chosen word

--- index.py
+++ index.py
@@ -1,8 +1,17 @@
 from random_word import RandomWords
 r = RandomWords()
 
-
 # Return a single random word
 randomWord = r.get_random_word()
 
 print(randomWord)
+
+# Ask player for a letter
+guess = input("Guess a letter: ").lower()
+
+# Print right or wrong depending on letter coincidence inside selected random word
+for letter in randomWord:
+    if letter == guess:
+        print("Right")
+    else:
+        print("Wrong")
