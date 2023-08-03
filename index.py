fff9c0a67168e821e5bcc05ba197e5f60bfe89c9
Veregorn <rjbarco@gmail.com>
2023-08-03 13:13:52 +0200

    Main loop and player lives implemented

--- index.py
+++ index.py
@@ -1,22 +1,112 @@
 from random_word import RandomWords
 r = RandomWords()
 
+# ASCII hangman art
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
 # Return a single random word
-randomWord = r.get_random_word()
+word = r.get_random_word()
+# Establish win condition value to false
+playerWins = False
+# Establish player lives
+playerLives = 6
 
 # Create a list with "_" instead of letters. Length must be the same as random word length
 display = []
-for i in range(0,len(randomWord)):
+wordLength = len(word)
+
+for _ in range(wordLength):
     display.append("_")
 
-print(f"Pssst, the solution is {randomWord}.")
+# we need a condition to check if all letters have been guessed
+allLettersGuessed = False
 
-# Ask player for a letter
-guess = input("Guess a letter: ").lower()
+print(f"Pssst, the solution is {word}.")
 
-# Print right or wrong depending on letter coincidence inside selected random word
-for i in range(0,len(randomWord)):
-    if randomWord[i] == guess:
-        display[i] = guess
+# This is the main game loop
+while not allLettersGuessed:
+    # Ask player for a letter
+    guess = input("Guess a letter: ").lower()
 
-print(display)
+    # Display letters guessed by player
+    notGuessed = True # This var controls if player has guessed some letter these round
+    for i in range(wordLength):
+        if word[i] == guess:
+            display[i] = guess
+            notGuessed = False
+            
+        print(display[i],end=" ")
+
+    # If player hasn't guessed the letter this round, subs a life
+    if notGuessed:
+        playerLives -= 1
+    
+    print("")
+
+    # Display game state
+    print(stages[playerLives])
+
+    # Test if all letters have been guessed
+    if "_" not in display:
+        allLettersGuessed = True
+        print("You win.")
+
+    # Test if game is over
+    if playerLives == 0:
+        print("You lose.")
+        break
